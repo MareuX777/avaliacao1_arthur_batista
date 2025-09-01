7af71dafea4f7f3b6da605f82ab6eb6d562560a6
MareuX777 <arthur.rodrigues43@aluno.ce.gov.br>
2025-09-01 12:09:52 -0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,17 +1,13 @@
 
 #Tem que rezar pra isso funcionar
-
+numero = 1
+media = 0
+acimaouigualnamedia = 0
+alunosrec = 0
 
 alunos = int(input("Quanto alunos tem na turma?: "))
 for n in range(alunos):
-    numero = 1
-    media = 0
-    acimaouigualnamedia = 0
-    alunosrec = 0
-     
     notas = float(input(f"Qual foi a nota do aluno? N°{numero}: "))
-    media += notas
-    numero += 1
     if notas >= 6:
         acimaouigualnamedia +=1
     else:
@@ -21,5 +17,6 @@ calculomdturma = media / alunos
 print(f"A média geral da turma: {calculomdturma}")
 print(f"Alunos de recuperação: {alunosrec}")
 print(f"Alunos na maior ou igual a média: {acimaouigualnamedia}")
+
 
 
